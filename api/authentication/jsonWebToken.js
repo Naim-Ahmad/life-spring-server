2cd74a1ba2f3c1574e57f92b7d6c2e7ad8280427
Naim Ahmad <naimahmad201103@gmail.com>
2023-12-18 11:48:29 +0600

    fixed login problem

--- api/authentication/jsonWebToken.js
+++ api/authentication/jsonWebToken.js
@@ -16,11 +16,12 @@ router.post("/createToken", async (req, res) => {
     const token = jwt.sign(req.body, process.env.JWT_SECRET, {
       expiresIn: "1h",
     });
-  
-    res.cookie('token', token, {
-      httpOnly: true,
-      secure: process.env.NODE_ENV === 'production',
-      sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+
+    res
+      .cookie("token", token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
       })
       .send({ message: "token created!" });
   } catch (error) {
@@ -46,10 +47,12 @@ router.delete("/deleteToken", async (req, res) => {
   }
 });
 
-router.get("/isActive", verifyToken, async (req, res) => {
+router.get("/isActive", async (req, res) => {
   try {
-    const userData = await User.findOne({ email: req?.decoded?.email });
-    res.send(userData.status === "active");
+    const userData = await User.findOne({ email: req?.query?.email });
+    const status = userData.status === "active";
+    console.log(status);
+    res.send(status);
   } catch (error) {
     res.status(500).send(error.message);
   }
--- api/test/test.js
+++ api/test/test.js
@@ -1,128 +1,128 @@
-const express = require('express')
+const express = require("express");
 
-const router = express.Router()
+const router = express.Router();
 
-const Test = require('../../models/Test')
-const verifyToken = require('../../middlewares/verifyToken')
-const verifyAdmin = require('../../middlewares/verifyAdmin')
+const Test = require("../../models/Test");
+const verifyToken = require("../../middlewares/verifyToken");
+const verifyAdmin = require("../../middlewares/verifyAdmin");
 
-router.get('/tests', async(req, res) => {
-    try {
-        // await Test.insertMany()
-        const tests = await Test.find()
-        res.send(tests)
-    } catch (error) {
-        res.status(500).send(error.message)
+router.get("/tests", async (req, res) => {
+  try {
+    // await Test.insertMany()
+    const tests = await Test.find();
+    res.send(tests);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+
+router.get("/tests/:id", async (req, res) => {
+  try {
+    const tests = await Test.findById(req.params.id);
+    res.send(tests);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+
+router.get("/getPageData", async (req, res) => {
+  try {
+    if (req.query?.date) {
+      const date = new Date(req.query.date);
+      console.log(date);
+      const query = { date: { $gt: date } };
+      const result = await Test.find(query);
+      return res.send(result);
     }
-})
 
-router.get('/tests/:id', async(req, res) => {
-    try {
-        const tests = await Test.findById(req.params.id)
-        res.send(tests)
-    } catch (error) {
-        res.status(500).send(error.message)
-    }
-})
+    const skip = req.query?.skip * 9;
 
-router.get('/getPageData', async(req, res) => {
-    try {
+    // console.log(skip, req.query?.skip);
 
+    // console.log(localDate);
+    const date = new Date();
+    // console.log(date);
 
-        if(req.query?.date){
-            const date = new Date(req.query.date)
-            console.log(date);
-            const query = {date: {$gt: date}}
-            const result = await Test.find(query)
-            return res.send(result)
-        }
-        const skip = req.query?.skip * 9;
+    const query = { date: { $gt: date } }; // Corrected the variable name to "date"
 
-        // console.log(skip, req.query?.skip);
+    const tests = await Test.find(query).skip(skip).limit(9);
+    console.log(tests);
+    res.send(tests);
+  } catch (error) {
+    res.status(500).send(error.message);
+    console.log(error);
+  }
+});
 
+router.get("/testCount", async (req, res) => {
+  try {
+    const date = new Date();
+    // console.log(date);
 
-        // console.log(localDate);
-        const date = new Date()
-        // console.log(date);
+    const query = { date: { $gt: date } }; // Corrected the variable name to "date"
 
-        const query = { date: { $gt: date } }; // Corrected the variable name to "date"
+    const tests = await Test.find(query);
+    const length = tests.length;
+    res.send({ count: length });
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
 
-        const tests = await Test.find(query).skip(skip).limit(9)
-        console.log(tests);
-        res.send(tests)
-    } catch (error) {
-        res.status(500).send(error.message)
-        console.log(error);
-    }
-})
+router.post("/tests", async (req, res) => {
+  try {
+    let test = new Test(req.body);
+    test = await test.save();
+    res.send(test);
+  } catch (error) {
+    res.status(500).send(error.message);
+    console.log(error);
+  }
+});
 
-router.get('/testCount', async(req, res) => {
-    try {
-        const date = new Date()
-        // console.log(date);
+router.put("/tests/:id", verifyToken, verifyAdmin, async (req, res) => {
+  try {
+    const test = await Test.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    res.send(test);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
 
-        const query = { date: { $gt: date } }; // Corrected the variable name to "date"
+router.delete("/tests/:id", verifyToken, verifyAdmin, async (req, res) => {
+  try {
+    const test = await Test.findByIdAndDelete(req.params.id);
+    res.send(test);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
 
-        const tests = await Test.find(query)
-        const length = tests.length;
-        res.send({count: length})
-    } catch (error) {
-        res.status(500).send(error.message)
-    }
-})
-
-router.post('/tests', async(req, res) => {
-    try {
-        let test = new Test(req.body)
-        test = await test.save()
-        res.send(test)
-    } catch (error) {
-        res.status(500).send(error.message)
-        console.log(error);
-    }
-})
-
-router.put('/tests/:id', verifyToken, verifyAdmin, async(req, res) => {
-    try {
-        const test = await Test.findByIdAndUpdate(req.params.id, req.body, {new: true})
-        res.send(test)
-    } catch (error) {
-        res.status(500).send(error.message)
-    }
-})
-
-router.delete('/tests/:id', verifyToken, verifyAdmin, async(req, res) => {
-    try {
-        const test = await Test.findByIdAndDelete(req.params.id)
-        res.send(test)
-    } catch (error) {
-        res.status(500).send(error.message)
-    }
-})
-
-router.get('/popularServices', async(req, res) => {
-    try {
-        const tests = await Test.aggregate([
-            {
-                $project: {
-                    // reservation: 1,
-                    testName: 1,
-                    description: 1, 
-                    imageURL:1,
-                    arrayLength: {$size: "$reservation"}
-                }
-            },
-            {
-                $sort: {arrayLength: -1} 
-            },
-            {
-                $limit: 6
-            }
-        ])
-        res.send(tests)
-    } catch (error) {
-        res.status(500).send(error.message)
-    }
-})
+router.get("/popularServices", async (req, res) => {
+  try {
+    const tests = await Test.aggregate([
+      {
+        $project: {
+          // reservation: 1,
+          testName: 1,
+          description: 1,
+          imageURL: 1,
+          arrayLength: { $size: "$reservation" },
+        },
+      },
+      {
+        $sort: { arrayLength: -1 },
+      },
+      {
+        $limit: 6,
+      },
+    ]);
+    res.send(tests);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
 
 module.exports = router;
